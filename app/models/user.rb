29e35cefdcd66ff8fcb87f608a0d8e6412b08317
Bogdan <kitz_99@yahoo.com>
2015-03-14 17:28:24 +0200

    User profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :uid, :first_name, :last_name
+  attr_accessible :uid, :first_name, :last_name, :iban, :bank, :email, :id
 
   def show_by_id(options={})
     User.where(id: options[:id])
