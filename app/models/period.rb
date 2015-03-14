078f96a8dcd260d19a47208e4cb96bedc6315d73
TeoDora <teodora.cernea@yahoo.com>
2015-03-14 18:09:21 +0200

    AddValidators

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,14 @@
 class Period < ActiveRecord::Base
 	attr_accessible :start, :end, :buget, :activ, :nr_stud, :min_salary
 	has_many :domain
+    
+    validates_associated :domain
+    validates :start, :end, :buget, :nr_stud, :min_salary, presence: true
+    validates :buget, numericality: { only_integer: true }
+    validates :min_salary, numericality: { only_integer: true}
+    validate :validate_dates
+
+    def validate_dates
+          errors.add(:eroare_perioada, ": Data de inceput trebuie sa fie precedenta datei de sfarsit") if :start > :end
+    end
 end
