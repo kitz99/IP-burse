bd81cfd9e57f7a08a5903053f42046c1471d6990
axintemihnea93 <axintemihnea@yahoo.com>
2015-03-18 17:44:17 +0200

    added template changes to news views

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -74,6 +74,7 @@ class NewsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_news
       @news = News.find(params[:id])
+      puts @news
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
