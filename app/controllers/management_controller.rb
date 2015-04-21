228bd31fd3b0a81ef52a1278f47e74d39b1e7a95
Bogdan <kitz_99@yahoo.com>
2015-04-21 17:49:15 +0300

    secretariat - in progress

--- app/controllers/management_controller.rb
+++ app/controllers/management_controller.rb
@@ -1,10 +1,20 @@
 class ManagementController < ApplicationController
   def index
   	@current_user = current_user
-  	@waiting = Application.where(:status => "In asteptare").length
+  	@waiting = build_hash Application.where(:status => "In asteptare").group(:scholarship_id)
 
     @accepted = Application.where(:status => "Aprobata").length
 
     @rejected = Application.where(:status => "Respinsa").length
   end
+
+  protected
+
+  	def build_hash(array)
+  		result = Hash.new
+  		sc_name = Scholarship.find_by(:id => array.first.scholarship_id).stype
+  		number = array.length
+  		{:sc_name => sc_name, :number => number}
+  	end
 end
+   
