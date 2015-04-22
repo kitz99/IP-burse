68774896d01ac53ea41eae7542a3212e7e25dbc4
Bogdan Boamfa <bogdan.boamfa@gmail.com>
2015-04-22 21:49:59 +0300

    Add staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,4 @@
+set :stage, :staging
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -7,7 +8,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
+set :rails_env, 'staging'
 
 # role-based syntax
 # ==================
@@ -49,13 +50,14 @@
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
+ server '193.226.51.18',
+   user: 'rails_burse',
+   roles: %w{web app db},
+   ssh_options: {
+     user: 'rails_burse', # overrides user setting above
+     keys: %w(/home/rails_burse/.ssh/id_rsa),
+     forward_agent: false,
+     port: 2022,
+     auth_methods: %w(publickey password)
+     # password: 'please use keys'
+   }
